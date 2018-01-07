c747dd0c910e472e011498e94e099471b80de580
smjcrane <sc989@cam.ac.uk>
2018-01-07 23:07:16 +0000

    created new files

---
+++ app/src/main/java/uk/ac/cam/sc989/notetimer/TakeNoteActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.sc989.notetimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_note);
+    }
+}
